5c42f6aba173afbdadb28ed49508faa100bd14ea
ZackDev <bastian.maier@gmx.de>
2021-03-19 13:10:35 +0100

    added comments

--- zackdev/background.js
+++ zackdev/background.js
@@ -1,8 +1,17 @@
+/*
+  Listener for cooke change events
+  - fetches all cookies from the browser's store
+  - then calls the update_action_text() function
+*/
 const cookie_event_listener = function cookie_event_listener( change_event ) {
   var get_all_cookies = browser.cookies.getAll( {} );
   get_all_cookies.then( update_action_text );
 };
 
+
+/*
+  updates the toolbar icon's badge text with the current number of cookies passed
+*/
 const update_action_text = function update_action_text( cookies ) {
   var num_cookies = String( cookies.length );
   browser.browserAction.setBadgeText(
@@ -12,20 +21,37 @@ const update_action_text = function update_action_text( cookies ) {
   );
 }
 
+
+/*
+  adds the Listener 'cookie_event_listener' to the cookies.onChanged event
+*/
 if ( !browser.cookies.onChanged.hasListener( cookie_event_listener ) ) {
   browser.cookies.onChanged.addListener( cookie_event_listener );
 }
 
+
+/*
+  sets the toolbar icon's badge text color
+*/
 browser.browserAction.setBadgeTextColor(
   {
     color: "#ffffff"
   }
 );
 
+
+/*
+  sets the toolbar icon's badge text background color
+*/
 browser.browserAction.setBadgeBackgroundColor(
   {
     color: "#000000"
   }
 );
 
+
+/*
+  initial call to the Listener 'cookie_event_listener', so we don't need to wait
+  for the event from cookies.onChanged
+*/
 cookie_event_listener( {} );
