01a3f062371363beaa70363bc7a4d2f6ca3edd6a
ZackDev <ZackDev@users.noreply.github.com>
2023-02-19 20:35:25 +0100

    cleanup

--- assets/modules/cookiesapi.mjs
+++ assets/modules/cookiesapi.mjs
@@ -39,121 +39,125 @@ class CookiesAPI {
      * @returns CookiesAPI instance
      */
     constructor() {
-        this.browser = '';
+        this.browserName = '';
         try {
-            this.setAPI('firefox', browser);
+            if (typeof browser !== 'undefined') {
+                this.setupAPI('firefox', browser)
+            }
+            if (typeof chrome !== 'undefined') {
+                this.setupAPI('chrome', chrome)
+            }
+            if (this.browserName !== '') {
+                this.initAPI();
+            }
+            else {
+                throw new Error('browser not supported');
+            }
+            console.log('browser deteced:', this.browserName)
             return this;
         }
-        catch (error) {}
-        try {
-            this.setAPI('chromium', chrome);
-            return this;
+        catch(error) {
+            console.log(error);
         }
-        catch (error) {}
     }
 
     /**
-     * determines the used browser and builds the cookiesAPI facade to partially uniform the usage of chromium's and firefox's WebAPI
-     * @param {String} b shorthand name of the browser 'firefox' or 'chromium' 
+     * determines the used browser and builds the cookiesAPI facade to partially uniform the usage of chrome's and firefox's WebAPI
+     * @param {String} b shorthand name of the browser 'firefox' or 'chrom' 
      * @param {Object} r the browser's top level access to the WebAPI 
      */
-    setAPI = (b, r) => {
-        if (this.browser === '') {
-            if (!r) {
-                throw new Error('API: entrypoint not found');
-            }
-            if (!r.browserAction) {
-                throw new Error('API: browserAction not found');
-            }
-            if (!r.cookies) {
-                throw new Error('API: cookies not found');
-            }
-            if (!r.downloads) {
-                throw new Error('API: downloads not found');
-            }
-            if (!r.runtime) {
-                throw new Error('API: runtime not found');
-            }
-            if (!r.storage) {
-                throw new Error('API: storage not found');
-            }
-            this.browserAction = r.browserAction;
-            this.cookies = r.cookies;
-            this.downloads = r.downloads;
-            this.filter = {
-                filterObj: {},
-                applyFilter: (cookies) => {
-                    var cookiesToDelete = [];
-                    switch (this.filter.filterObj.ss) {
-                        case 'allowlist': {
-                            cookiesToDelete = cookies.filter((c) => {
-                                return !this.filter.filterObj.fa.includes(c.domain);
-                            });
-                            break;
-                        }
-                        case 'denylist': {
-                            cookiesToDelete = cookies.filter((c) => {
-                                return this.filter.filterObj.fd.includes(c.domain);
-                            });
-                            break;
-                        }
-                    }
-                    cookiesToDelete.forEach((c) => {
-                        this.remove(new StackCookie(c));
-                    });
-                },
-                initStorage: () => {
-                    this.getValue(null)
-                        .then((storageObject) => {
-                            if (!storageObject.ss) {
-                                storageObject.ss = 'disabled';
-                            }
-                            if (!storageObject.fa) {
-                                storageObject.fa = [];
-                            }
-                            if (!storageObject.fd) {
-                                storageObject.fd = [];
-                            }
-                            this.storeValue(storageObject);
+    setupAPI = (b, r) => {
+        if (!r) {
+            throw new Error('API: entrypoint not found');
+        }
+        if (!r.browserAction) {
+            throw new Error('API: browserAction not found');
+        }
+        if (!r.cookies) {
+            throw new Error('API: cookies not found');
+        }
+        if (!r.downloads) {
+            throw new Error('API: downloads not found');
+        }
+        if (!r.runtime) {
+            throw new Error('API: runtime not found');
+        }
+        if (!r.storage) {
+            throw new Error('API: storage not found');
+        }
+        this.browserAction = r.browserAction;
+        this.cookies = r.cookies;
+        this.downloads = r.downloads;
+        this.filter = {
+            filterObj: {},
+            applyFilter: (cookies) => {
+                var cookiesToDelete = [];
+                switch (this.filter.filterObj.ss) {
+                    case 'allowlist': {
+                        cookiesToDelete = cookies.filter((c) => {
+                            return !this.filter.filterObj.fa.includes(c.domain);
                         });
-                },
-                initFilter: () => {
-                    this.getValue(null)
-                        .then((storageObject) => {
-                            this.filter.filterObj = storageObject;
-                        });
-                },
-                updateFilter: (storageObject) => {
-                    let keys = Object.keys(storageObject);
-                    for (let key of keys) {
-                        switch (key) {
-                            case 'fa':
-                                if (storageObject.fa.newValue) {
-                                    this.filter.filterObj.fa = storageObject.fa.newValue;
-                                }
-                                break;
-                            case 'fd':
-                                if (storageObject.fd.newValue) {
-                                    this.filter.filterObj.fd = storageObject.fd.newValue;
-                                }
-                                break;
-                            case 'ss':
-                                if (storageObject.ss.newValue) {
-                                    this.filter.filterObj.ss = storageObject.ss.newValue;
-                                }
-                                break;
-                        }
+                        break;
                     }
-                    this.getAll({}, this.filter.applyFilter);
+                    case 'denylist': {
+                        cookiesToDelete = cookies.filter((c) => {
+                            return this.filter.filterObj.fd.includes(c.domain);
+                        });
+                        break;
+                    }
                 }
+                cookiesToDelete.forEach((c) => {
+                    this.remove(new StackCookie(c));
+                });
+            },
+            initStorage: () => {
+                this.getValue(null)
+                    .then((storageObject) => {
+                        if (!storageObject.ss) {
+                            storageObject.ss = 'disabled';
+                        }
+                        if (!storageObject.fa) {
+                            storageObject.fa = [];
+                        }
+                        if (!storageObject.fd) {
+                            storageObject.fd = [];
+                        }
+                        this.storeValue(storageObject);
+                    });
+            },
+            initFilter: () => {
+                this.getValue(null)
+                    .then((storageObject) => {
+                        this.filter.filterObj = storageObject;
+                    });
+            },
+            updateFilter: (storageObject) => {
+                let keys = Object.keys(storageObject);
+                for (let key of keys) {
+                    switch (key) {
+                        case 'fa':
+                            if (storageObject.fa.newValue) {
+                                this.filter.filterObj.fa = storageObject.fa.newValue;
+                            }
+                            break;
+                        case 'fd':
+                            if (storageObject.fd.newValue) {
+                                this.filter.filterObj.fd = storageObject.fd.newValue;
+                            }
+                            break;
+                        case 'ss':
+                            if (storageObject.ss.newValue) {
+                                this.filter.filterObj.ss = storageObject.ss.newValue;
+                            }
+                            break;
+                    }
+                }
+                this.getAll({}, this.filter.applyFilter);
             }
-            this.keyNamePairs = keyNamePairs;
-            this.runtime = r.runtime;
-            this.storage = r.storage;
         }
-        else {
-            return;
-        }
+        this.keyNamePairs = keyNamePairs;
+        this.runtime = r.runtime;
+        this.storage = r.storage;
 
         switch (b) {
             case 'firefox':
@@ -178,13 +182,9 @@ class CookiesAPI {
                 this.getValue = (v) => {
                     return this.storage.local.get(v);
                 }
-                this.storage.onChanged.addListener(this.filter.updateFilter);
-                this.filter.initStorage();
-                this.filter.initFilter();
-                this.getAll({}, this.filter.applyFilter);
                 break;
 
-            case 'chromium':
+            case 'chrome':
                 this.remove = (stack_cookie) => {
                     let details = {
                         name: stack_cookie.cookie.name,
@@ -194,7 +194,7 @@ class CookiesAPI {
                     this.cookies.remove(details);
                 }
                 this.getAll = this.cookies.getAll;
-                this.browserAction.setBadgeTextColor = () => { };
+                this.browserAction.setBadgeTextColor = () => {};
                 this.storeValue = (v) => {
                     return new Promise((res) => {
                         this.storage.local.set(v, (r) => {
@@ -209,11 +209,18 @@ class CookiesAPI {
                         });
                     });
                 }
-                this.storage.onChanged.addListener(this.filter.updateFilter);
-                this.filter.initStorage();
-                this.filter.initFilter();
-                this.getAll({}, this.filter.applyFilter);
                 break;
         }
+        this.browserName = b;
+    }
+
+    /**
+     * 
+     */
+    initAPI() {
+        this.storage.onChanged.addListener(this.filter.updateFilter);
+        this.filter.initStorage();
+        this.filter.initFilter();
+        this.getAll({}, this.filter.applyFilter);
     }
 }
