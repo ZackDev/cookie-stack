b41e496f2feca7120f2d3e11830a8e709ffe5239
ZackDev <ZackDev@users.noreply.github.com>
2023-02-20 12:23:32 +0100

    cleanup

--- assets/modules/helper.mjs
+++ assets/modules/helper.mjs
@@ -1,26 +1,26 @@
 export { Helper }
 
 /*
-  Helper class
-  - static uniquString( str )
+  Helper
+  - uniquString( str )
   -- returns the concatenated ascii-number representation of the passed string
   -- solely for generating valid HTML-Element selectors from strings that contain special
   -- characters like '.' and '#'
   -- negative example 'github.com' as selector would select tags named 'github' with class 'com'
   
-  - static checkOrX( b )
+  - checkOrX( b )
   -- returns HTML-glyph '&check;' or '&cross;', based on the parameter b
 */
-class Helper {
-    static uniqueString(str) {
+
+const Helper = {
+    uniqueString(str) {
         let u_str = '';
         for (let c of str) {
             u_str += c.charCodeAt(0);
         }
         return u_str;
-    }
-    static checkOrX(b) {
-        let str = b ? '&check;' : '&cross;';
-        return str;
+    },
+    checkOrX(bool) {
+        return bool ? '&check;' : '&cross;';
     }
 }
